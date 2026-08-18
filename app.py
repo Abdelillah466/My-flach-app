8fd94e0b0507332fccdb84433f872ce21a63970d
Abdelillah466 <abdelilahhadjslimen@gmail.com>
2026-08-18 23:24:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+# الصفحة الرئيسية
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
+# مسار استقبال البيانات والتحليل
+@app.route('/analyze', methods=['POST', 'GET'])
+def analyze():
+    if request.method == 'POST':
+        # هنا يمكنك استقبال البيانات من الفورم
+        return render_template('index.html')
+    return render_template('index.html')
+
+if __name__ == '__main__':
     app.run()
